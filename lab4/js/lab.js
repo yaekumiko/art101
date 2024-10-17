4972a063d0dc41ad5316252466cd1830f90701fe
yaekumiko <mikifos03@gmail.com>
2024-10-17 14:17:39 -0700

    Made edits to lab 4 (JS, HTML, CSS), correcting links in labs section

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,68 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Connect-Four
+// An implementation of the game
+// Miki Foster <tamfoste@ucsc.edu> and Precious Garcia <prngarci@ucsc.edu>
+// 16 October 2024
 
-// Constants
+// Task1 : Pseucode an everyday task (Making spaghetti)
+// 1. Gather ingredients and tools
+      // Pasta noodles, olive oil, minced garlic, marinara sauce, water, salt, desired spices/herbs, ground beef
+      // Pot, saute pan, utensils (tea spoon, wooden spatula, strainer, pasta spoon)
+// 2. Boil water
+      // Fill pot with water
+      // Add a couple pinches of salt to water
+      // On high heat, allow water to boil
+// 3. Add pasta
+      // When water comes to a rolling boil, add pasta
+      // Stir noodles occasionally
+      // Allow noodles to cook for 10-12 minutes
+// 4. Prepare saute pan for ground beef
+      // Place saute pan on stove
+      // On low to medium heat, pour a teaspoon of oil onto saute pan
+      // Add a teaspoon of minced garlic for taste
+      // Allow pan to heat up
+// 5. Cook ground beef
+      // Add ground beef to the pan
+      // Using a wooden spatula, break beef into smaller chunks/pieces
+      // Continue to break down the beef as it cooks, stirring occasionally
+// 6. Season ground beef
+      // Add your choice of seasonings; salt, pepper, italian seasoning, garlic salt
+      // Stir to combine
+// 7. Add marinara sauce
+      // Lower heat on stove for saute pan
+      // Pour sauce into saute pan
+      // Stir to combine sauce and beef
+      // Allow it to simmer
+// 8. Turn off pan
+// 9. Drain pasta noodles
+      // In a strainer, drain pasta noodles from pot
+      // Stir noodles to prevent sticking
+// 10. Serve food
+      // Scoop noodles into bowl
+      // Top noodles with beef/marinara sauce
+// 11. Enjoy your meal!
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Task 2: Pseudocode a simple computer game
+// 1. print board before each turn
+      // create 6x7 grid
+            // loop through each row
+                  // loop through each space
+      // set all positions to empty
+// 2. start game loop
+      // if no winner and board is empty:
+            // print current board
+            // player makes their move
+// 3. determine player turn
+    // player 1 - red, player 2 - yellow
+    // ask player to choose a column
+      // check if column is empty
+            // if column is valid, drop piece to the lowest space in row
+            // if column is invalid, ask player to pick again
+            // loop through each turn
+// 5. check for win or draw condition
+      // horizontal
+      // veritical
+      // diagonal
+      // if all spaces are filled with no winner, declare draw
+// 6. end game
+      // announce winner or draw
