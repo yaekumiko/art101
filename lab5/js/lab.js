5123bfc2dd0747ce8260784e6130a3980a9d4916
yaekumiko <mikifos03@gmail.com>
2024-10-21 20:30:58 -0700

    Made changes to Lab 5 CSS, JS, HTML, and uploaded an image

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * 
+ * Author: Miki Foster
+ * Created: 21 October 2024
+ * License: Public Domain
+ */
 
-// Constants
+// Define Variables
+make = "Lexus";
+model = "is250 F Sport";
+color = "Black";
+year = 2014;
 
-// Functions
+// calculate
+age = 2024 - year
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
