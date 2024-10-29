1dfd98f265397219e394f7925f5925108281478b
yaekumiko <mikifos03@gmail.com>
2024-10-28 21:40:27 -0700

    made a lot of changes to things

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,27 +1,31 @@
 // Author: Miki Foster
 // Created: 28 October 2024
 
-// declare variable userName
-// use window.prompt() to get user's name
-function sortUserName {
-    var userName=window.prompt("Enter your name:");
-    console.log("userName =", userName);
+function myFunction(param1, param2) {
 
-    // split string to array
-    var nameArray = nameArray.split.('');
-    console.log("nameArray =", nameArray);
-
-    // sort array
-    var nameArraySort = nameArraySort.sort('');
-    console.log("nameArraySort =", nameArraySort);
-
-    // join array back to string
-    var nameSorted = nameArraySort.join();
-    console.log("nameSorted =", nameSorted);
-    
-    return nameSorted;
 }
 
-// result/output
-document.writeln("Here is your new name: ",
+function main() {
+    console.log("Main function started.");
+}
+
+// sortUsername function that takes user input and sorts the letters
+function sortUserName() {
+    var userName=window.prompt("Enter your name: ");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArray=userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort array
+    var nameArraySort=nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    // join array to a string
+var nameSorted=nameArraySort.join('');
+console.log("nameSorted =", nameSorted);
+
+return nameSorted;
+}
+
+// output
+document.writeln("Here's your new name: ",
     sortUserName(), "</br>");
