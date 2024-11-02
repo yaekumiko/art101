0de78d209992a3d12a407dc1d824e1bdb0c026f3
yaekumiko <mikifos03@gmail.com>
2024-11-01 23:34:58 -0700

    created lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,10 @@
+/* 
+    lab.js - this JavaScript/jQuery script uses buttons to modify elements on the page
+
+    Requirements: jQuery must be loaded for this script to work
+
+    Author: Miki Foster
+    Created: 1 November 2024
+
+*/
+
