34a2240c6e4dc8cd06c6a04f73b284354bf292a2
yaekumiko <mikifos03@gmail.com>
2024-11-20 20:38:51 -0800

    made changes to lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,23 +7,57 @@
     Created: 6 November 2024
 */
 
-function generateRandomText() {
-    const text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const min=3;
-    const max=200;
-    const randLen=Math.floor(Math.random() * (max - min + 1)) + min;
-// get a random string index to slice the lorem ipsum text
-    const randStart=Math.floor(Math.random() * (text.length - randLen + 1));
-// generate random lorem ipsum-like text
-    return text.slice(randStart, randStart + randLen);
-}
+    const lyrics=`I don't go out, but I'll do it for you
+You never liked it when I drink too much
+I hate to dance, but I'd dance with you
+'Cause I'd do anything to feel your touch
+Don't like anybody, tell me why it's different with you
+Don't believe in love, but no one makes me feel like you do
+I don't say it much 'cause I just always thought
+That you knew, oh
+It's what you do to me
+I'm wrapped around your finger and I can't stop
+You know I got a soft spot for you
+You know I got a soft spot for you
+Baby, can't you see?
+I need you 'cause you're everything that I'm not
+You know I got a soft spot for you
+You know I got a soft spot for you
+Too late, don't wanna fall, baby, I just
+Don't need somebody else to throw me aside
+But I'm up all night, thinkin' 'bout how
+It could be you to change my heart, but I
+Don't like anybody, tell me why it's different with you
+Don't believe in love, but no one makes me feel like you do
+I don't say it much 'cause I just always thought that
+You knew, oh
+It's what you do to me
+I'm wrapped around your finger and I can't stop
+You know I got a soft spot for you
+You know I got a soft spot for you
+Baby, can't you see?
+I need you 'cause you're everything that I'm not
+You know I got a soft spot for you
+You know I got a soft spot for you
+For you
+For you
+For you
+You know I got a soft spot for you
+It's what you do to me
+I'm wrapped around your finger and I can't stop
+You know I got a soft spot for you
+You know I got a soft spot for you`;
 
-// click listener for button
-$("#make-convo").click(function(){
 
-// new fake dialogue
-    const newText=generateRandomText();
+const lyricsArray = lyrics.trim().split('\n');
+let currentIndex = 0;
 
-// append new div to output div
-    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
-});
+$("#make-convo").click(function() {
+    if (currentIndex < lyricsArray.length) {
+        const currentLine = lyricsArray[currentIndex].trim();
+
+        $("#output").append('<div class="text"><p>' + currentLine + '</p></div>');
+
+        currentIndex++;
+    }
+})
